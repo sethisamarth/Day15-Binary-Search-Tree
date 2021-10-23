d201a4417c3c15a749eacc86f7d379e659ac8316
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-23 22:57:26 +0530

    Creating Binary Tree

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -6,7 +6,40 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Binary Search Tree!!!");
+            Console.WriteLine("*********BINARY SEARCH TREE OPERATIONS*********");
+            Console.WriteLine("1.Insertion.");
+            Console.WriteLine("2.Insertion full BT.");
+            Console.WriteLine("Enter the option!!!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            BST<int> binarySearchTree = new BST<int>(56);
+            switch (num)
+            {
+                case 1:
+                    binarySearchTree.Insert(30);
+                    binarySearchTree.Insert(70);
+                    binarySearchTree.Display();
+                    break;
+                case 2:
+                    binarySearchTree.Insert(30);
+                    binarySearchTree.Insert(70);
+                    binarySearchTree.Insert(22);
+                    binarySearchTree.Insert(40);
+                    binarySearchTree.Insert(11);
+                    binarySearchTree.Insert(3);
+                    binarySearchTree.Insert(16);
+                    binarySearchTree.Insert(60);
+                    binarySearchTree.Insert(95);
+                    binarySearchTree.Insert(65);
+                    binarySearchTree.Insert(63);
+                    binarySearchTree.Insert(67);
+                    binarySearchTree.Display();
+                    binarySearchTree.GetSize();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid option!!!");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
